c6780bc8a320cf19f00cdcd2b6a750a97cb3b9f4
Neil <h66r10@gmail.com>
2020-07-19 21:30:16 +0200

    added direct seed finding

--- content/en/shadow_seed/_index.md
+++ content/en/shadow_seed/_index.md
@@ -9,8 +9,7 @@ resources:
   params:
     byline: "Photo: Riona MacNamara / CC-BY-CA"
 ---
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bignumber.js/9.0.0/bignumber.min.js" integrity="sha512-vg7OeB3i21ZSV8k2kyAXnbFL24Rwoz+nRC6xVAddaoEQIIboCtONdpuQQ+tl56RBDEf8fT0DtcVQeip2asslmg==" crossorigin="anonymous"></script>
-<script src="./shadow.js"></script>
+
 <link rel="stylesheet" href="./shadow.css">
 {{< blocks/cover class="shadow-cover"  >}}
 <div class="mx-auto">
@@ -18,10 +17,10 @@ resources:
     
 <input id="seed" type="text" class="form-control " placeholder="Your current seed" aria-label="Get your shadow seed" aria-describedby="basic-addon2">
 <div class="input-group-append">
-<button class="btn btn-primary" type="button" onclick="shadow()">Get your shadow</button>
+<button id="getIt" class="btn btn-primary" type="button" onclick="shadow()">Get your shadow</button>
 </div>
 </div> 
-
+<div id="warning" class="warning mb-3 mx-auto "></div>
 <div class="mb-3 mx-auto display-result">   
 <div onClick="selectText(this);" onselect="selectText(this);" class="vw-50 result-text" id="result" style=""></div>
 <button id="button_clipboard" class="btn btn-primary" type="button" onclick="copy(this,'result');" style="display: none">Copy to clipboard</button>
@@ -32,6 +31,7 @@ resources:
 
 {{< /blocks/cover >}}
 
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bignumber.js/9.0.0/bignumber.min.js" integrity="sha512-vg7OeB3i21ZSV8k2kyAXnbFL24Rwoz+nRC6xVAddaoEQIIboCtONdpuQQ+tl56RBDEf8fT0DtcVQeip2asslmg==" crossorigin="anonymous"></script>
+<script src="./shadow.js"></script>
 
     
--- content/en/shadow_seed/shadow.js
+++ content/en/shadow_seed/shadow.js
@@ -11,8 +11,9 @@ const negI63 = new BigNumber("-9223372036854775808");
 
 
 function shadow() {
+    document.getElementById("warning").textContent = "";
     let seed = document.getElementById("seed").value;
-    if ( seed===null || seed===""){
+    if (seed === null || seed === "") {
         return;
     }
     // check if it is a string
@@ -24,30 +25,32 @@ function shadow() {
     // check if it is a 64 bit one else hash it
     if (seed.comparedTo(I63) >= 0 || seed.comparedTo(negI63) < 0) {
         seed = new BigNumber(hashCode(seed.toString()));
+        document.getElementById("warning").textContent = "This seed is out of the range, it will be treated as a string!"
     }
     // get the solution as -7379792620528906219 - seed but make sure its a positive 64 bit value
     let solution = new BigNumber('-7379792620528906219').minus(seed);
-    solution=solution.modulo(I64);
+    solution = solution.modulo(I64);
     // if should be signed, sign it
     if (solution.comparedTo(I63) >= 0) {
-        solution=solution.minus(I64).negated()
+        solution = solution.minus(I64).negated()
     }
     while (solution.comparedTo(negI63) < 0) {
-        solution=solution.plus(I64)
+        solution = solution.plus(I64)
     }
     document.getElementById("result").textContent = solution.toString();
-    document.getElementById("button_clipboard").style.display="inline-block";
-    document.getElementById("button_clipboard").textContent="Copy to clipboard";
+    document.getElementById("button_clipboard").style.display = "inline-block";
+    document.getElementById("button_clipboard").textContent = "Copy to clipboard";
 }
 
-function copy(el,id) {
+function copy(el, id) {
     var copyText = document.getElementById(id);
     /* Select the text field */
     selectText(copyText);
     /* Copy the text inside the text field */
     document.execCommand("copy");
-    el.textContent="Copied!"
+    el.textContent = "Copied!"
 }
+
 function selectText(el) {
     let range;
     if (document.selection) { // IE
@@ -61,3 +64,21 @@ function selectText(el) {
         window.getSelection().addRange(range);
     }
 }
+
+const dispatchForCode = function (event, callback) {
+    let code;
+    if (event.key !== undefined) {
+        code = event.key;
+    } else if (event.keyIdentifier !== undefined) {
+        code = event.keyIdentifier;
+    } else if (event.keyCode !== undefined) {
+        code = event.keyCode;
+    }
+    callback(code);
+};
+document.getElementById("seed").addEventListener("keyup", function (event) {
+    dispatchForCode(13, () => {
+        event.preventDefault();
+        document.getElementById("getIt").click();
+    })
+});
